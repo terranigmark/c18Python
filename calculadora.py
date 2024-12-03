024a6443cebbec25fb9d49a265b2743ca5e1686d
terranigmark <linnk99@gmail.com>
2024-12-02 18:22:54 -0700

    Agregadas las funciones restar, multiplicar y dividir en calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,3 +2,18 @@
 
 def sumar(x, y):
     return x + y
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
+
